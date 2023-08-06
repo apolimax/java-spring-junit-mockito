c4753c7058d1d6eaa4f7b7a7d59c2a3bcc25aa17
Will Apolinário <willieapolinario@gmail.com>
2023-08-06 17:35:42 -0300

    ModelMapper, UserDTO

--- src/main/java/com/apiexample/api/resources/UserResource.java
+++ src/main/java/com/apiexample/api/resources/UserResource.java
@@ -1,5 +1,6 @@
 package com.apiexample.api.resources;
 
+import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -7,17 +8,20 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import com.apiexample.api.domain.User;
+import com.apiexample.api.domain.dto.UserDTO;
 import com.apiexample.api.services.UserService;
 
 @RestController
 @RequestMapping(value = "/users")
 public class UserResource {
     @Autowired
+    private ModelMapper mapper;
+
+    @Autowired
     private UserService service;
 
     @GetMapping("/{id}")
-    public ResponseEntity<User> findById(@PathVariable Integer id) {
-        return ResponseEntity.ok().body(service.findById(id));
+    public ResponseEntity<UserDTO> findById(@PathVariable Integer id) {
+        return ResponseEntity.ok().body(mapper.map(service.findById(id), UserDTO.class));
     }
 }
