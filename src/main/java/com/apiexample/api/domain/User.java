5783b3f7230c95baa65d0d9260fe53d268c2b738
Will Apolinário <willieapolinario@gmail.com>
2023-08-06 15:14:35 -0300

    User domain class

---
+++ src/main/java/com/apiexample/api/domain/User.java
@@ -0,0 +1,31 @@
+package com.apiexample.api.domain;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+//@Getter
+//@Setter
+@Data // = @Getter + @Setter + @EqualsAndHashCode + @ToString +
+      // @RequiredArgsConstructor
+@AllArgsConstructor
+@NoArgsConstructor
+// @EqualsAndHashCode
+// @ToString
+@Entity
+@Table(name = "tb_user")
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+    private String name;
+
+    @Column(unique = true)
+    private String email;
+}
